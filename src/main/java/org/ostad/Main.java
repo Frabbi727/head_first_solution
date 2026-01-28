eb1b8f38eb9003994828e2146e74e78acd2e7adb
Rabbi <fazle.rabbi@bracits.com>
2026-01-28 23:11:57 +0600

    solve 63-B the Object has no referance

--- src/main/java/org/ostad/Main.java
+++ src/main/java/org/ostad/Main.java
@@ -1,34 +1,27 @@
 package org.ostad;
 
-class Books {
-    String title;
-    String author;
-}
+class Hobbits {
+    String name;
 
-class BooksTestDrive {
     public static void main(String[] args) {
-        Books[] myBooks = new Books[3];
-        int x = 0;
+        Hobbits[] h = new Hobbits[3];
+        int z = 0;
 
-        // STEP 1: Create the objects
-        myBooks[0] = new Books();
-        myBooks[1] = new Books();
-        myBooks[2] = new Books();
+        while (z < 4) {
+            System.out.println("add of z=" +z);
+            z = z + 1;
+            System.out.println("value of z=" +z);
 
-        myBooks[0].title = "The Grapes of Java";
-        myBooks[1].title = "The Java Gatsby";
-        myBooks[2].title = "The Java Cookbook";
-        myBooks[0].author = "bob";
-        myBooks[1].author = "sue";
-        myBooks[2].author = "ian";
-
-
-        while (x < 3) {
-            System.out.print(myBooks[x].title);
-            System.out.print(" by ");
-            System.out.println(myBooks[x].author);
-            x = x + 1;
+            h[z] = new Hobbits();
+            h[z].name = "bilbo";
+            if (z == 1) {
+                h[z].name = "frodo";
+            }
+            if (z == 2) {
+                h[z].name = "sam";
+            }
+            System.out.print(h[z].name + " is a ");
+            System.out.println("good Hobbit name");
         }
-
     }
 }
